5f9e0c3c3e3a1961b6f425a0ed1dc43c6c22b363
Grace Shih <vocalise@gmail.com>
2018-09-10 01:39:53 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,7 +27,11 @@ class SongsController < Sinatra::Base
       genre = Genre.create(name: params['Genre Name'])
       @song.genres << genre
     end
+<<<<<<< HEAD
     genre_ids = params['genres']
+=======
+    genre_ids = params['genre_ids']
+>>>>>>> 7ac881a468f3a5239997cc5527511a7d8fbc1fa3
     if !genre_ids.nil?
       genre_ids.each do |g_id|
         genre = Genre.find_by(id: g_id)
@@ -58,5 +62,24 @@ class SongsController < Sinatra::Base
   end
 
 
+<<<<<<< HEAD
 
+=======
+  patch '/songs/:slug' do
+    binding.pry
+    @song = Song.find_by_slug(params[:slug])
+    @song.update(params['Song Name'])
+    artist = Artist.find_by_slug(params['Artist Name'])
+    if  !artist.nil?
+      artist_obj = Artist.create(name: params['Artist Name'])
+      @song.artists << artist_obj
+    else
+      @song.artist = artist
+    end
+    
+    
+    flash[:message] = "Successfully updated song."
+    redirect to "/songs/#{@song.slug}"
+  end
+>>>>>>> 7ac881a468f3a5239997cc5527511a7d8fbc1fa3
 end
